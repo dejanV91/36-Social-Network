e769f9f3879c1740fa28066d03f130e5ad073d01
dejanV91 <dejanvukoicic91@gmail.com>
2022-10-26 19:45:16 +0200

    initial commit

--- js/Post.js
+++ js/Post.js
@@ -4,4 +4,27 @@ class Post{
     user_id = "";
     likes = "";
     api_url = "https://6334a608ea0de5318a06d722.mockapi.io";
+
+    async create(){
+        let session = new Session();
+        session_id = session.getSession();
+
+        let data = {
+            user_id: session_id,
+            content: this.post_content,
+            likes: 0
+        }
+
+        data = JSON.stringify(data);
+
+        let response = await fetch(this.api_url + "posts", {
+            method: "POST",
+            headers: {
+                "Content-Type" : "application/json"
+            },
+            body: data
+        });
+        data = await response.json();
+        return data;
+    }
 }
